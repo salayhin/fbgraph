7cc1c5c1dddb9b8ddce11c4f25acff758d4bde57
salayhin <salayhin@gmail.com>
2013-09-18 17:39:16 +0600

    Debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,35 @@
+require "net/http"
+require "net/https"
+require "cgi"
+require "openssl"
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Executes an HTTP GET request.
+  # It raises a RuntimeError if the response code is not equal to 200
+  def http_get host, path, params
+    connection = Net::HTTP.new(host)
+    process_http_response connection.request_get(path + "?" + to_query_string(params))
+  end
+
+  # Executes an HTTP POST request over SSL
+  # It raises a RuntimeError if the response code is not equal to 200
+  def https_post host, path, params
+    https_connection host do |connection|
+      connection.request_post(path, to_query_string(params))
+    end
+  end
+
+  # Executes an HTTP GET request over SSL
+  # It raises a RuntimeError if the response code is not equal to 200
+  def https_get host, path, params, headers = nil
+    https_connection host do |connection|
+      connection.request_get(path + "?" + to_query_string(params), headers)
+    end
+  end
 
   protected
 
